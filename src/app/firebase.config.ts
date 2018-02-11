60796f5fbae382be2b1ff666269a8e8d381a55b3
Ing1412 <ings59@email.nu.ac.th>
2018-02-11 11:41:01 +0700

    initial

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyD017e-iDAxDARCJd0QFrglf8ccMRa_we0',
+    authDomain: 'speak-5325c.firebaseapp.com',
+    databaseURL: 'https://speak-5325c.firebaseio.com',
+    projectId: 'speak-5325c',
+    storageBucket: 'speak-5325c.appspot.com',
+    messagingSenderId: '1077507007999'
+}
